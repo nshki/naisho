4042e65dc36045537f02aeadf3622b7e87cf0651
Nishiki Liu <hello@nshki.com>
2024-06-22 12:40:05 -0700

    Use new method in sync methods

--- app/models/concerns/company/california_data_brokers_requestable.rb
+++ app/models/concerns/company/california_data_brokers_requestable.rb
@@ -18,16 +18,16 @@ module Company::CaliforniaDataBrokersRequestable
       registry_csv = Net::HTTP.get(REGISTRY_CSV_URI)
 
       CSV.parse(registry_csv, headers: true, row_sep: "\r\n") do |row|
-        email = row["Business primary contact email address"]
-        name = row["Business name"]
         website = row["Business primary website"]
-        next if email.blank? || name.blank? || website.blank?
+        name = row["Business name"]
+        email = row["Business primary contact email address"]
+        next if website.blank? || name.blank? || email.blank?
 
-        company = Company.find_or_initialize_by(email: email)
-        company.update \
-          category: Company::CATEGORIES[:california_data_broker],
+        upsert_by_website \
+          website: website,
           name: name,
-          website: website
+          email: email,
+          category: Company::CATEGORIES[:california_data_broker]
       rescue ActiveRecord::RecordNotUnique
       end
     end
