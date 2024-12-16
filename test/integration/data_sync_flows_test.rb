992dd506c69e61e4b4c14f4748f295a04b3cb828
Nishiki Liu <hello@nshki.com>
2024-12-15 23:02:07 -0800

    Add `Company.sync_all` method

--- test/integration/data_sync_flows_test.rb
+++ test/integration/data_sync_flows_test.rb
@@ -1,21 +1,10 @@
 require "test_helper"
 
 class DataSyncFlowsTest < ActionDispatch::IntegrationTest
-  test "CPPA data brokers syncable" do
+  test "all companies syncable" do
     assert_equal 1, Company.count # We start off with one fixture
 
-    Company.update_california_data_brokers
-
-    assert Company.count > 1
-    assert_equal 0, Company.where("name like ?", " %").count
-    assert_equal 0, Company.where("name like ?", "% ").count
-    assert_equal 0, Company.where("website like ?", "%www%").count
-  end
-
-  test "DataBrokersWatch.org companies syncable" do
-    assert_equal 1, Company.count # We start off with one fixture
-
-    Company.update_data_brokers_watch_companies
+    Company.sync_all
 
     assert Company.count > 1
     assert_equal 0, Company.where("name like ?", " %").count
